fe2034a0fd68f03d7fd2d9bf4086c36e0f47b29c
nicstewart0022 <nicstewart0022@gmail.com>
2021-11-19 10:12:13 -0700

    Moved forecast html into JS

--- src/app.js
+++ src/app.js
@@ -22,6 +22,34 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+       <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+            <img
+               src="http://openweathermap.org/img/wn/02d@2x.png"
+                alt=""
+                width="40"
+                />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperatures-max">18°</span>
+                <span class="weather-forecast-temperatures-min">12°</span>
+              </div>
+          </div>
+     `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -86,3 +114,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 search("New York");
+displayForecast();
